cd514a165220228968720435e2edcda5c1be7bcb
plan-out <45305408+plan-out@users.noreply.github.com>
2025-12-10 09:10:30 +0900

    Add files via upload

--- 웹디자인개발기능사/A-3/script/script.js
+++ 웹디자인개발기능사/A-3/script/script.js
@@ -7,4 +7,12 @@ setInterval(()=>{
   $(".view"+(i++)).fadeIn().siblings().fadeOut();
 },3000);
 
+/*버튼클릭시 팝업창 나타나기*/
+$(".open").on("click",()=>{
+  $("#popup").show();
+});
 
+/*버튼클릭시 팝업창 사라지기*/
+$(".close").on("click",()=>{
+  $("#popup").hide();
+});
